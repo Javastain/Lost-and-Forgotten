ac9a2567d964c52800e1e50c8f34c7de3c4c5e95
Xyganoth <cyrusjnedwards@gmail.com>
2026-05-03 03:45:45 -0500

    I did a little bit more :)

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,20 +6,30 @@ pygame.mixer.init()
 flags = pygame.SCALED | pygame.RESIZABLE
 screen = pygame.display.set_mode((1244, 900), flags)
 pygame.display.set_caption('Eleventh Hour')
-outside = pygame.image.load("Assets/Image Assets/Lost And Forgotten.png").convert_alpha()
-outside = pygame.transform.scale(outside, (1244, 900))
 clock = pygame.time.Clock()
 running = True
 
 
-backdrop = pygame.image.load('Assets/Image Assets/Computer Backdrop.png')
-login_screen = pygame.image.load('Assets/Image Assets/Login Screen.png')
+backdrop = pygame.image.load('Assets/Image Assets/Computer Backdrop.png').convert_alpha()
+login_screen = pygame.image.load('Assets/Image Assets/Login Screen.png').convert_alpha()
+nicole_pfp = pygame.image.load('Assets/Image Assets/Nicole pfp.png').convert_alpha()
+michael_pfp = pygame.image.load('Assets/Image Assets/Michael pfp.png').convert_alpha()
+john_pfp = pygame.image.load('Assets/Image Assets/John pfp.png').convert_alpha()
+marion_pfp = pygame.image.load('Assets/Image Assets/Marion pfp.png').convert_alpha()
+outside = pygame.image.load("Assets/Image Assets/Lost And Forgotten.png").convert_alpha()
+exit_icon = pygame.image.load("Assets/Image Assets/Exit Icon.png").convert_alpha()
+mail_icon = pygame.image.load("Assets/Image Assets/Mail Icon.png").convert_alpha()
+notes_icon = pygame.image.load("Assets/Image Assets/Notes Icon.png").convert_alpha()
+photos_icon = pygame.image.load("Assets/Image Assets/Photos Icon.png").convert_alpha()
+
+outside = pygame.transform.scale(outside, (1244, 900))
 
 power_hitbox = pygame.Rect((908,780),(33,33))
 nicole_hitbox = pygame.Rect((305,260),(200,70))
 michael_hitbox = pygame.Rect((305,360),(200,70))
 john_hitbox = pygame.Rect((305,460),(200,70))
 marion_hitbox = pygame.Rect((305,560),(200,70))
+exit_hitbox = pygame.Rect((285,665),(60,60))
 
 computer_on = False
 account = "no one"
@@ -27,6 +37,7 @@ account_selected = "no one"
 mail_open = False
 notes_open = False
 photos_open = False
+hint = False
 
 password_input = ""
 password_active = False
@@ -40,14 +51,25 @@ screen.blit(outside, (0, 0))
 header = pygame.font.SysFont('tahoma', 25)
 body_text = pygame.font.SysFont('tahoma', 12)
 
+
 login_prompt = header.render('To begin, click on your profile', True, (255, 255, 255))
 login_prompt_rect = login_prompt.get_rect()
 login_prompt_rect.center = (740, 550)
+
+password_prompt = body_text.render('Password:', True, (255,255,255))
+password_prompt_rect = password_prompt.get_rect()
+password_prompt_rect.center = (650, 525)
+
+hint_prompt = body_text.render('Need a hint?', True, (255,255,255))
+hint_prompt_rect = password_prompt.get_rect()
+hint_prompt_rect.center = (650, 585)
+
 password_input_rect = pygame.Rect((625, 540), (250, 30))
 
 
 # This would draw a rectangle that perfectly fits over the screen. Just so we know its dimensions.
 # pygame.draw.rect(screen, "white", pygame.Rect(622,450,0,0).inflate(690,570))
+# pygame.draw.rect(screen, "white", pygame.Rect(277,165,690,570)
 
 
 while running:
@@ -96,56 +118,85 @@ while running:
             computer_on = True
 
 
-        if computer_on == True and account == "no one" and account_selected == "no one":
+        if computer_on == True and account == "no one":
             screen.blit(login_prompt, login_prompt_rect)
             
             
             if event.type == pygame.MOUSEBUTTONDOWN and nicole_hitbox.collidepoint(point):
                 account_selected = "nicole"
+                screen.blit(nicole_pfp,(710, 300))
             if event.type == pygame.MOUSEBUTTONDOWN and michael_hitbox.collidepoint(point):
                 account_selected = "michael"
+                screen.blit(michael_pfp,(710, 300))
             if event.type == pygame.MOUSEBUTTONDOWN and john_hitbox.collidepoint(point):
                 account_selected = "john"
+                screen.blit(john_pfp,(710, 300))
             if event.type == pygame.MOUSEBUTTONDOWN and marion_hitbox.collidepoint(point):    
                 account_selected = "marion"
+                screen.blit(marion_pfp,(710, 300))
 
-        if account_selected != "no one":
-            pygame.draw.rect(screen, (103,133,206), ((277+290,165+320),(350,207)))            
+        if account == "no one" and account_selected != "no one":
+            pygame.draw.rect(screen, (103,133,206), ((277+290,165+320),(350,207)))
+
             if event.type == pygame.MOUSEBUTTONDOWN and password_input_rect.collidepoint(point):
                 password_active = True
             
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN and password_active == True:
                 if event.key == pygame.K_BACKSPACE:
                     password_input = password_input[:-1]
                     text = body_text.render(password_input, True, (255, 255, 255))
                 if event.key == pygame.K_RETURN:
                     password_active = False
+                    
+                    if account_selected == 'nicole':
+                        account = 'nicole'
+                    if account_selected == 'michael' and password_input == 'test!':
+                        account = 'michael'
 
-            if event.type == pygame.TEXTINPUT:
+                    if account != 'no one':
+                        pygame.time.delay(900)
+                        screen.blit(backdrop,(276,165))
+                        screen.blit(exit_icon,(285,665))
+                        screen.blit(outside,(0,0))
+                    password_input = ''
+
+            if event.type == pygame.TEXTINPUT and password.get_width() < 240 and password_active == True:
                 password_input += event.text
-
-            """
-            # if the key is physically pressed down
-            if event.type == pygame.TEXTINPUT:
-                if event.text == pygame.K_BACKSPACE:
-                    # stores text except last letter
-                    password_input = password_input[0:-1]
-                else:
-                    pygame.key.set_repeat(1000,500)
-                    password_input += event.text
-            """
 
             if password_active:
                 password_color = password_color_active
             else:
                 password_color = password_color_passive
-        
-            pygame.draw.rect(screen, password_color, password_input_rect)
 
+            if account == "no one":
+                screen.blit(password_prompt, password_prompt_rect)
+                pygame.draw.rect(screen, password_color, password_input_rect)
+                screen.blit(hint_prompt, hint_prompt_rect)
+            
             # Renders the text
             password = body_text.render(password_input, True, (0, 0, 0))
             screen.blit(password, (password_input_rect.x + 5, password_input_rect.y + 5))
-            password_input_rect.w = max(100, password.get_width() + 10)
+
+
+            if hint_prompt_rect.collidepoint(point) and mouse_left:
+                hint = True
+            if hint:
+                if account_selected == "nicole":
+                    hint_prompt = body_text.render('Need a hint?\nIt\'s the first thing you think of!', True, (255,255,255))
+                if mouse_left and not hint_prompt_rect.collidepoint(point):
+                    hint_prompt = body_text.render('Need a hint?', True, (255,255,255))
+                    hint = False
+            
+
+        if account != "no one":
+            # pygame.draw.rect(screen, (103,133,206), exit_hitbox)
+            if exit_hitbox.collidepoint(point) and mouse_left:
+                account = "no one"
+                account_selected = "no one"
+                pygame.time.delay(900)
+                screen.blit(login_screen,(276,165))
+                screen.blit(outside,(0,0))
+
             
     # pygame.draw.rect(screen, "white", (287,655,70,70))
     
