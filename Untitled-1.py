39fb129ad33301427aa2434c293b955f8afcf421
Xyganoth <cyrusjnedwards@gmail.com>
2026-05-02 20:09:43 -0500

    Computer can turn on!!
    it can do that

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,20 @@
 import pygame
 
 pygame.init()
+pygame.mixer.init()
 flags = pygame.SCALED | pygame.RESIZABLE
 screen = pygame.display.set_mode((1244, 900), flags)
 background = pygame.image.load("Assets/Image Assets/Lost And Forgotten.png").convert_alpha()
 background = pygame.transform.scale(background, (1244, 900))
 clock = pygame.time.Clock()
 running = True
+
+
+backdrop = pygame.image.load('Assets/Image Assets/Computer Backdrop.png')
+
+screen.fill('#302732')
+screen.blit(background, (0, 0))
+
 
 while running:
     # poll for events
@@ -16,18 +24,28 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill('#302732')
-    screen.blit(background, (0, 0))
+    # screen.fill('#302732')
+    # screen.blit(background, (0, 0))
     # RENDER YOUR GAME HERE
 
     
     
     # Mouse pointer
     point = pygame.mouse.get_pos()
+    # Get mouse button states
+    mouse_left, mouse_middle, mouse_right = pygame.mouse.get_pressed()
 
     # On button
-    power_hitbox = pygame.Rect((1000,800),(1050,850))
-    pygame.draw.rect(screen, '#FFFFFF', power_hitbox)
+    power_hitbox = pygame.Rect((908,780),(33,33))
+    if power_hitbox.collidepoint(point) and mouse_left:
+        pygame.mixer.music.load("Assets/Audio Assets/braammmm.wav")
+        pygame.mixer.music.play()
+        
+        pygame.time.delay(3000)
+        screen.blit(backdrop,(100,100))
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
 
 
     
